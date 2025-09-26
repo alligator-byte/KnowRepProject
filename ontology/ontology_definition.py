66ed8775f8c061602c756b20218b1cbdcc998541
Luke <lbvido22@gmail.com>
2025-09-26 08:55:20 +0800

    Create ontology_definition.py
    Added comment on what should be done to get started

---
+++ ontology/ontology_definition.py
@@ -0,0 +1,6 @@
+"""
+should use OWL2Ready to define:
+- Repository, Branch, Commit, User, File
+- Properties: hasBranch, hasCommit, authoredBy, modifiesFile, hasParent, etc.
+- Datatype properties: timestamp, commitMessage, branchName
+"""
