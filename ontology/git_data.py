b2712f58df4cb5d11741a49d70cf454335b7458c
Luke <23814635@student.uwa.edu.au>
2025-10-13 13:50:01 +0800

    "Bortalis/3403-Agile" added to JSONS

--- ontology/git_data.py
+++ ontology/git_data.py
@@ -22,7 +22,7 @@ FETCH_DEFAULT_ONLY = str(os.getenv("FETCH_DEFAULT_ONLY", "0")).lower() in ("1", 
 
 #modify this to be a set of related repositories. after cmdr2 the rest are other repos we get info from
 REPOS = [
-    "Donaks1/gsa"
+    
 ]
 
 later = [    "ShahDhruvik/dietRecommendationFrontend",
@@ -31,7 +31,6 @@ later = [    "ShahDhruvik/dietRecommendationFrontend",
     "ShahDhruvik/nextjs13",
     "ShahDhruvik/my-portfolio-site",
     "ShahDhruvik/my-portfolio-backend",
-    "Donaks1/gsa",
     "Donaks1/gsauwa",
     "Donaks1/Hangman",
     "Donaks1/testagit",
@@ -42,7 +41,7 @@ later = [    "ShahDhruvik/dietRecommendationFrontend",
     "emilyand-i/AgileWebGroup82",
     "alligator-byte/Group-7-Embedded-Systems-2025",
     "Bortalis/Networks",
-    "Bortalis/3403-Agile",
+    
     "tri-tim-m-nguyen/study-plan-application",
     "AndrewMekhail/Project_132",]
 
@@ -50,6 +49,8 @@ complete = ["KOBE24MAN/aaa",
             "codersforcauses/guild-volunteering",
             "cmdr2/github-actions-wizard",
             "alligator-byte/Group-7-Embedded-Systems-2025",
+            "Donaks1/gsa",
+            "Bortalis/3403-Agile",
             ]
 
 OUTPUT_FILES = {
