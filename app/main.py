296012e8c96576f0e81473b5a17730e57e9ccb7b
Luke <23814635@student.uwa.edu.au>
2025-10-14 12:01:41 +0800

    Flask template and basic code added

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
 # This file and folder is for Flask for later
+from flask import Flask, render_template, request 
+from owlready2 import get_ontology
+import os
+
+app = Flask(__name__)
+
+#Load the ontology made in our previous steps
+ONTO_PATH = os.path.join(os.path.dirname(__file__), "..", "git_knowledge_graph.owl")
+onto = get_ontology(ONTO_PATH).load()
+
+@app.route("/")
+def index():
+    #List all repos
+    repos = [r for r in onto.individuals() if "Repository" in r.is_a[0].name]
+    return render_template("index.html",repos =repos)
+
+if __name__ == "__main__":
+    app.run(debug=True)
